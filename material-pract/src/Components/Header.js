137ae3d4546c92fc635ab7e15faa4c5fe4ebf475
Andrew <32229265+aselee@users.noreply.github.com>
2018-08-28 13:22:50 -0500

    adding m icons

--- material-pract/src/Components/Header.js
+++ material-pract/src/Components/Header.js
@@ -1,18 +1,20 @@
 import React from 'react';
-// import AppBar from '@material-ui/core/AppBar';
-// import Toolbar from '@material-ui/core/Toolbar';
 import { 
   AppBar, 
   Toolbar,
   Typography
 } from '@material-ui/core';
+import CreateDialog from '../Exercises/Dialogs/Create';
+// import AppBar from '@material-ui/core/AppBar';
+// import Toolbar from '@material-ui/core/Toolbar';
 
 
 export default props =>
 <AppBar position="static">
   <Toolbar>
-  <Typography variant="headline" color="inherit">
+  <Typography variant="headline" color="inherit" style={{flex: 1}}>
         Exercise Database
   </Typography>
+  <CreateDialog/> 
   </Toolbar>
 </AppBar>

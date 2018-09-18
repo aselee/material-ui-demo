0b73b30cded1c6d974f08298a140752330f41912
Andrew <32229265+aselee@users.noreply.github.com>
2018-09-17 19:28:05 -0500

    toolbar issue

--- material-template/src/App.js
+++ material-template/src/App.js
@@ -1,21 +1,85 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
+import { withStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import SearchIcon from '@material-ui/icons/Search';
+import Button from '@material-ui/core/Button';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+// Do I need const express? Check past projects
+// const express = require("express");
+
+
+const styles = theme => ({
+  layout: {
+    width: 'auto', 
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+      width: 1100,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  toolbarMain: {
+    borderBottom: `1px solid ${theme.palette.grey[300]}`,
+  },
+  toolbarTitle: {
+    flex: 1,
+  },
+  toolbarSecondary: {
+    justifyContent: 'space-between',
   }
-}
+});
 
-export default App;
+const sections = [
+  'About',
+  'Projects',
+  'Social',
+  'Contact'
+];
+
+export default class extends Component {
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <Toolbar className={classes.toolbarMain}>
+          <Button size="small">Subscribe</Button>
+          <Typography
+            variant="headline"
+            color="inherit"
+            align="center"
+            noWrap
+            className={classes.toolbarTitle}
+          >
+            Blog
+          </Typography>
+          <IconButton>
+            <SearchIcon />
+          </IconButton>
+          <Button variant="outlined" size="small">
+            Sign up
+          </Button>
+        </Toolbar>
+        <Toolbar variant="dense" className={classes.toolbarSecondary}>
+          {sections.map(section => (
+            <Typography color="inherit" noWrap key={section}>
+              {section}
+            </Typography>
+          ))}
+        </Toolbar>
+      </React.Fragment>
+    )
+  }
+};
+
+Component.propTypes = {
+  classes: PropTypes.object.isRequired,
+};

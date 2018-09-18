659c9b26ad6e36a8137210282d376ca8ff898625
Andrew <32229265+aselee@users.noreply.github.com>
2018-09-18 02:51:22 -0500

    card update

--- material-template/src/App.js
+++ material-template/src/App.js
@@ -1,17 +1,20 @@
-import React, { Component, Fragment } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import './App.css';
-import { withStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import SearchIcon from '@material-ui/icons/Search';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import { withStyles } from '@material-ui/core/styles';
 
 // Do I need const express? Check past projects
 // const express = require("express");
 
+
+// trying to see if we can add image in the card.
+// work on  that next.
 
 const styles = theme => ({
   layout: {
@@ -32,7 +35,18 @@ const styles = theme => ({
   },
   toolbarSecondary: {
     justifyContent: 'space-between',
-  }
+  },
+  mainFeaturedPost: {
+    backgroundColor: theme.palette.grey[800],
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing.unit * 4,
+  },
+  mainFeaturedPostContent: {
+    padding: `${theme.spacing.unit * 6}px`,
+    [theme.breakpoints.up('md')]: {
+      paddingRight: 0,
+    },
+  },
 });
 
 const sections = [
@@ -42,44 +56,60 @@ const sections = [
   'Contact'
 ];
 
-export default class extends Component {
-
-  render() {
-    const { classes } = this.props;
+function Template(props) {
+  const { classes } = props;
 
     return (
       <React.Fragment>
         <CssBaseline />
-        <Toolbar className={classes.toolbarMain}>
-          <Button size="small">Subscribe</Button>
-          <Typography
-            variant="headline"
-            color="inherit"
-            align="center"
-            noWrap
-            className={classes.toolbarTitle}
-          >
-            Blog
-          </Typography>
-          <IconButton>
-            <SearchIcon />
-          </IconButton>
-          <Button variant="outlined" size="small">
-            Sign up
-          </Button>
-        </Toolbar>
-        <Toolbar variant="dense" className={classes.toolbarSecondary}>
-          {sections.map(section => (
-            <Typography color="inherit" noWrap key={section}>
-              {section}
+        <div className={classes.layout}>
+          <Toolbar className={classes.toolbarMain}>
+            <Button size="small">Button1</Button>
+            <Typography
+              variant="headline"
+              color="inherit"
+              align="center"
+              noWrap
+              className={classes.toolbarTitle}
+            >
+              Webportfolio Template
             </Typography>
-          ))}
-        </Toolbar>
+            <Button variant="outlined" size="small">
+              Button2
+            </Button>
+          </Toolbar>
+          <Toolbar variant="dense" className={classes.toolbarSecondary}>
+            {sections.map(section => (
+              <Typography color="inherit" noWrap key={section}>
+                {section}
+              </Typography>
+            ))}
+          </Toolbar>
+          <main>
+            {/* Pic of me and title */}
+            <Card className={classes.mainFeaturedPost}>
+              <Grid container>
+                <Grid item md={6}>
+                  <div className={classes.mainFeaturedPostContent}>
+                    <Typography variant="display2" color="inherit" gutterBottom>
+                      title of something here
+                    </Typography>
+                    <Typography variant="headline" color="inherit" paragraph>
+                      Multiple of lines text here probably wont use this? 
+                      Not really sure but adding it for testing.
+                    </Typography>
+                    <Typography variant="title" color="inherit">
+                      continue reading this...
+                    </Typography>
+                  </div>
+                </Grid>
+              </Grid>
+            </Card>
+          </main>
+            
+        </div>
       </React.Fragment>
     )
-  }
 };
 
-Component.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+export default withStyles(styles)(Template);
